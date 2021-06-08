692c915e64b714ef6dd9b341b2c7aa4e0fe9b721
mk87751 <mk87751@gmail.com>
2021-06-08 13:29:43 +0530

    nav bar added

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+function Nav() {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else setShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_log "
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+        alt="Netflix logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
